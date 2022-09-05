ec73c3e35c26269b102f78bc969118f61e03054b
shahariyer.as <shahariyer.as@gmail.com>
2022-09-05 11:59:05 +0600

    cart add

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,41 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import Product from '../Product/Product';
-import './Shop.css';
+import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    useEffect( () =>{
-        fetch('products.json')
-        .then(res=> res.json())
-        .then(data => setProducts(data))
-    }, []);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    const handleAddToCart = (product) =>{
-        console.log(product);
-        // do not do this: cart.push(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
-    }
+  const handleAddToCart = (product) => {
+    // console.log(product);
+    // do not do this: cart.push(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+    console.log(cart);
+  };
 
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product=><Product 
-                        key={product.id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                        ></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;

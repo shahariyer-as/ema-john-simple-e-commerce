f926f64119c5b8ccb09e0b4b4b49b215ac87ecc0
shahariyer.as <shahariyer.as@gmail.com>
2022-09-05 18:50:29 +0600

    total fixed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -28,13 +28,20 @@ const Shop = () => {
     setCart(savedCard);
   }, [products]);
 
-  const handleAddToCart = (product) => {
-    // console.log(product);
-    // do not do this: cart.push(product);
-    const newCart = [...cart, product];
+  const handleAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find((product) => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
     setCart(newCart);
     // local storage database add
-    addToDb(product.id);
+    addToDb(selectedProduct.id);
   };
 
   return (

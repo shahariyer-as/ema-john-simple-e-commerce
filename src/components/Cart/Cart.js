bb3a8970047d1dfaaf69b54522cc6e3609d950d8
shahariyer.as <shahariyer.as@gmail.com>
2022-09-05 18:29:41 +0600

    stored product

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+  const { cart } = props;
+  let total = 0;
+  let shipping = 0;
+  // let quantity = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
   return (
-    <div>
+    <div className="cart">
       <h4>Order Summary</h4>
       <p>Selected Items: {cart.length}</p>
+      <p>Total price: ${total} </p>
+      <p>Total shipping: ${shipping}</p>
+      <p>Tax:{tax}</p>
+      <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -6,19 +7,34 @@ import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-
+  // data fetch
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  // local storage
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    const savedCard = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCard.push(addedProduct);
+      }
+    }
+    setCart(savedCard);
+  }, [products]);
 
   const handleAddToCart = (product) => {
     // console.log(product);
     // do not do this: cart.push(product);
     const newCart = [...cart, product];
     setCart(newCart);
-    console.log(cart);
+    // local storage database add
+    addToDb(product.id);
   };
 
   return (

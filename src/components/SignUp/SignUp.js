ed66883399d5045efe9f633eec57a580003d2a79
shahariyer.as <shahariyer.as@gmail.com>
2022-09-10 11:02:54 +0600

    signup with firebase email password

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,94 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const SignUp = () => {
-    return (
-        <div>
-            SignUp
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  // set signup info handle to use state
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleConfirmPasswordBlur = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+  // user create check
+  if (user) {
+    navigate("/shop");
+  }
+
+  const handleCrateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("your two password didn't match");
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("password must be 6 character longer");
+      return;
+    }
+    createUserWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <div className="form-container">
+      <div>
+        <form onSubmit={handleCrateUser} action="">
+          <h1 className="form-title">Sign Up</h1>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm-Password</label>
+            <input
+              onBlur={handleConfirmPasswordBlur}
+              type="password"
+              name="confirm-password"
+              id=""
+              required
+            />
+          </div>
+          <p style={{ color: "red" }}>{error}</p>
+          <input className="form-submit" type="submit" value="SignUp" />
+        </form>
+        <p>
+          Already have an account?
+          <Link className="form-link" to="/login">
+            Please login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,12 +1,14 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQjXIAWY_1ToBc_XVF68fqTYMK0J836bw",
-    authDomain: "ema-john-simple-a7ce2.firebaseapp.com",
-    projectId: "ema-john-simple-a7ce2",
-    storageBucket: "ema-john-simple-a7ce2.appspot.com",
-    messagingSenderId: "970800767249",
-    appId: "1:970800767249:web:d173faa9557cfaeaa8ecb0"
+  apiKey: "AIzaSyCQjXIAWY_1ToBc_XVF68fqTYMK0J836bw",
+  authDomain: "ema-john-simple-a7ce2.firebaseapp.com",
+  projectId: "ema-john-simple-a7ce2",
+  storageBucket: "ema-john-simple-a7ce2.appspot.com",
+  messagingSenderId: "970800767249",
+  appId: "1:970800767249:web:d173faa9557cfaeaa8ecb0",
 };
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;

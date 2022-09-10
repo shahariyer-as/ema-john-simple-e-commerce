3e5a99e74ae105fd2a3983bc5a780bcc1d2117bf
shahariyer.as <shahariyer.as@gmail.com>
2022-09-10 16:01:37 +0600

    require auth add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import "./Login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
@@ -20,7 +22,7 @@ const Login = () => {
   };
 
   if (user) {
-    navigate("/shop");
+    navigate(from, { replace: true });
   }
   const handleUserSignIn = (event) => {
     event.preventDefault();

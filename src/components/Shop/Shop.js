6f5a9298ba503054675493a47bd5b755435e28ab
shahariyer.as <shahariyer.as@gmail.com>
2022-09-20 00:00:49 +0600

    pagination data load

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,10 +7,18 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const [products, setProducts] = useState([]);
+  // products load
+  useEffect(() => {
+    fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [page, size]);
+
   // page count
   useEffect(() => {
     fetch("http://localhost:5000/productCount")
@@ -75,8 +83,17 @@ const Shop = () => {
               {number + 1}
             </button>
           ))}
+          <select onClick={(e) => setSize(e.target.value)}>
+            <option value="5">5</option>
+            <option value="10" selected>
+              10
+            </option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
         </div>
       </div>
+
       <div className="cart-container">
         <Cart cart={cart}>
           <Link to="/orders">
